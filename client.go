2fae88daaa3ed70a4490e42efd7dec1057c700ce
Mikelis Asnins <masnins@meteomatics.com>
2026-01-08 13:17:39 +0100

    Add support for post request and bearer auth

--- client.go
+++ client.go
@@ -9,16 +9,25 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 )
 
 // DefaultBaseURL is the default base URL.
 const DefaultBaseURL = "https://api.meteomatics.com"
+
+type TransportMethod string
+
+const (
+	MethodGet  TransportMethod = http.MethodGet
+	MethodPost TransportMethod = http.MethodPost
+)
 
 // A Client is a Client.
 type Client struct {
 	httpClient      *http.Client
 	baseURL         string
 	preRequestFuncs []func(*http.Request)
+	method          TransportMethod
 }
 
 // A ClientOption sets an option on a Client.
@@ -32,6 +41,10 @@ type RequestOptions struct {
 	ClusterSelect         string
 	Timeout               int
 	Route                 bool
+}
+
+type TransportOptions struct {
+	Method string // http.MethodGet, http.MethodPost
 }
 
 // WithBaseURL sets the base URL.
@@ -57,11 +70,27 @@ func WithBasicAuth(username, password string) ClientOption {
 	}
 }
 
+// WithBearerAuth adds bearer token header
+func WithBearerAuth(token string) ClientOption {
+	return func(c *Client) {
+		c.preRequestFuncs = append(c.preRequestFuncs, func(req *http.Request) {
+			req.Header.Add("Authorization", fmt.Sprintf("bearer %s", token))
+		})
+	}
+}
+
+func WithTransportMethod(method TransportMethod) ClientOption {
+	return func(c *Client) {
+		c.method = method
+	}
+}
+
 // NewClient returns a new Client with options set.
 func NewClient(options ...ClientOption) *Client {
 	c := &Client{
 		httpClient: http.DefaultClient,
 		baseURL:    DefaultBaseURL,
+		method:     MethodGet,
 	}
 	for _, o := range options {
 		o(c)
@@ -72,29 +101,37 @@ func NewClient(options ...ClientOption) *Client {
 // Request performs a raw request. It is the caller's responsibility to
 // interpret the []byte returned.
 func (c *Client) Request(ctx context.Context, ts TimeStringer, ps ParameterStringer, ls LocationStringer, fs FormatStringer, options *RequestOptions) ([]byte, error) {
-	urlStr := fmt.Sprintf("%s/%s/%s/%s/%s", c.baseURL, ts.TimeString(), ps.ParameterString(), ls.LocationString(), fs.FormatString())
-	if values := options.Values(); values != nil {
-		urlStr += "?" + values.Encode()
-	}
+	var (
+		req *http.Request
+		err error
+	)
 
-	req, err := http.NewRequest(http.MethodGet, urlStr, nil)
+	switch c.method {
+	case http.MethodPost:
+		req, err = c.buildPostRequest(ctx, ts, ps, ls, fs, options)
+	default:
+		req, err = c.buildGetRequest(ctx, ts, ps, ls, fs, options)
+	}
 	if err != nil {
 		return nil, err
 	}
-	req = req.WithContext(ctx)
-	req.Header.Set("Accept", fs.ContentType())
-	for _, f := range c.preRequestFuncs {
-		f(req)
-	}
 
+	return c.do(req)
+}
+
+func (c *Client) do(req *http.Request) ([]byte, error) {
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
+	respBody, readErr := io.ReadAll(resp.Body)
+	if readErr != nil {
+		return nil, readErr
+	}
+
 	if resp.StatusCode < http.StatusOK || http.StatusMultipleChoices <= resp.StatusCode {
-		respBody, _ := io.ReadAll(resp.Body)
 		return nil, &Error{
 			Request:      req,
 			Response:     resp,
@@ -102,7 +139,61 @@ func (c *Client) Request(ctx context.Context, ts TimeStringer, ps ParameterStrin
 		}
 	}
 
-	return io.ReadAll(resp.Body)
+	return respBody, nil
+}
+
+func (c *Client) buildGetRequest(ctx context.Context, ts TimeStringer, ps ParameterStringer, ls LocationStringer, fs FormatStringer, options *RequestOptions) (*http.Request, error) {
+	urlStr := fmt.Sprintf("%s/%s/%s/%s/%s",
+		c.baseURL,
+		ts.TimeString(),
+		ps.ParameterString(),
+		ls.LocationString(),
+		fs.FormatString(),
+	)
+
+	if values := options.Values(); values != nil {
+		urlStr += "?" + values.Encode()
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, urlStr, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	c.applyCommonHeaders(req, fs)
+	c.applyPreRequest(req)
+	return req, nil
+}
+
+func (c *Client) buildPostRequest(ctx context.Context, ts TimeStringer, ps ParameterStringer, ls LocationStringer, fs FormatStringer, options *RequestOptions) (*http.Request, error) {
+	// URL: /<validdatetime>/
+	urlStr := fmt.Sprintf("%s/%s/", c.baseURL, ts.TimeString())
+
+	// Body: <parameters>/<location>/<format>?<optionals>
+	bodyStr := fmt.Sprintf("%s/%s/%s", ps.ParameterString(), ls.LocationString(), fs.FormatString())
+	if values := options.Values(); values != nil {
+		bodyStr += "?" + values.Encode()
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, urlStr, strings.NewReader(bodyStr))
+	if err != nil {
+		return nil, err
+	}
+
+	c.applyCommonHeaders(req, fs)
+	req.Header.Set("Content-Type", "text/plain; charset=utf-8")
+	c.applyPreRequest(req)
+	return req, nil
+}
+
+func (c *Client) applyPreRequest(req *http.Request) {
+	for _, f := range c.preRequestFuncs {
+		f(req)
+	}
+}
+
+func (c *Client) applyCommonHeaders(req *http.Request, fs FormatStringer) {
+	req.Header.Set("Accept", fs.ContentType())
 }
 
 // Values returns the url.Values that set the request options defined by o.
--- client_test.go
+++ client_test.go
@@ -10,12 +10,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestClientRequestError(t *testing.T) {
+func TestClientGetRequestError(t *testing.T) {
 	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 	}))
 
-	_, err := NewClient(WithBaseURL(s.URL)).Request(
+	_, err := NewClient(WithBaseURL(s.URL), WithTransportMethod(MethodPost)).Request(
 		context.Background(),
 		TimeNow,
 		Parameter{
@@ -32,4 +32,28 @@ func TestClientRequestError(t *testing.T) {
 	)
 	require.Error(t, err)
 	assert.Equal(t, s.URL+"/now/t_2m:C/0,190/csv: 404 Not Found", err.Error())
+}
+
+func TestClientPostRequestError(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	}))
+
+	_, err := NewClient(WithBaseURL(s.URL), WithTransportMethod(MethodPost)).Request(
+		context.Background(),
+		TimeNow,
+		Parameter{
+			Name:  ParameterTemperature,
+			Level: LevelMeters(2),
+			Units: UnitsCelsius,
+		},
+		Point{
+			Lat: 0,
+			Lon: 190,
+		},
+		FormatCSV,
+		nil,
+	)
+	require.Error(t, err)
+	assert.Equal(t, s.URL+"/now/: 404 Not Found", err.Error())
 }
